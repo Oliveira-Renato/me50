a199cf1a3254a6f7e7f20097ec4c66eb4656fa5b
Renato Oliveira <renato.printf@gmail.com>
2021-08-16 23:00:13 -0300

    in progress

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
+teste = ['CSS','Django','Git','HTML','Python']
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def title(request):
+    return render(request,"encyclopedia/title.html", {
+        "titles": markdown2.markdown(util.get_entry(teste[0]))
     })
 
