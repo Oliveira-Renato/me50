cb9b387e6e4eba66a4ef22171d9c15d2ee354a1a
Renato-Teste <renato_fortuna@outlook.com>
2021-08-19 16:05:56 -0300

    in progress

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,9 @@
-from django.http import request, HttpResponse
+from django.http import request, HttpResponse, Http404
 from django.shortcuts import render
+from django import forms
 import markdown2
 from . import util
 
-
-#https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Django/Generic_views
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -12,12 +11,39 @@ def index(request):
     })
 
 
+
 def title(request, name):
-    if name not in 'LISTA AQUI':
-        return HttpResponse('PAGE NOT FOUND')
-    else:
-        return render(request,"encyclopedia/title.html", {
-        "content": markdown2.markdown(util.get_entry(name)),
-        "name": name.upper()
+    if request.method == 'POST':
+        content = util.get_entry(name)
+        return render(request, "encyclopedia/title.html", {
+            "title": name,
+            "content": content
+        })
+
+    for item in util.list_entries():
+        if name.lower() == item.lower():
+            name = item
+
+    if not util.get_entry(name):
+        raise Http404("Page does not exist")
+    
+    page = markdown2.markdown(util.get_entry(name))#convert the MD tex into HTML
+    return render(request, "encyclopedia/title.html", {
+        "title":name,
+        "content": page
     })
 
+
+
+
+
+
+    #for  item in util.list_entries() :
+     #   if name.lower() == item.lower():
+     #        return render(request,"encyclopedia/title.html", {
+     #       "content": markdown2.markdown(util.get_entry(name)),
+    #      "name": name.upper()
+    #    })
+     #   else:
+      #      return Http404('PAGINA NÃO ENCONTRADA!')
+    
