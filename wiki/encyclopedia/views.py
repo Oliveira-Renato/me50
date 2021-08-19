6ab56072a9d6353782e58342c36d15f4aa1d4bbf
Renato-Teste <renato_fortuna@outlook.com>
2021-08-19 10:24:46 -0300

    in progress

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,13 +4,19 @@ import markdown2
 from . import util
 
 
+#https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Django/Generic_views
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries":util.list_entries()
     })
 
+
 def title(request, name):
-    return render(request,"encyclopedia/title.html", {
+    if name not in 'LISTA AQUI':
+        return HttpResponse('PAGE NOT FOUND')
+    else:
+        return render(request,"encyclopedia/title.html", {
         "content": markdown2.markdown(util.get_entry(name)),
         "name": name.upper()
     })
